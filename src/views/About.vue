b86a43dcdc03c75e27e07e8e42e91d7a5f0f16e3
周必川 <zhoubichuan@icloud.com>
2023-08-20 19:08:00 +0800

    update

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,74 @@
 <template>
   <div class="about">
     <h1>这里是基座about页面，页面基于vue3.x开发</h1>
+    <div class="content">
+      <a href="https://zhoubichuan.github.io/web-vue/">vue</a>
+      <a href="https://zhoubichuan.github.io/web-react/">react</a>
+      <a href="https://zhoubichuan.github.io/web-webpack/">webpack</a>
+      <a href="https://zhoubichuan.github.io/web-ionic/">ionic</a>
+      <a href="https://zhoubichuan.github.io/web-rollup/">rollup</a>
+      <a href="https://zhoubichuan.github.io/web-electorn/">electorn</a>
+      <a href="https://zhoubichuan.github.io/web-elementui/">elementui</a>
+      <a href="https://zhoubichuan.github.io/web-openlayers/">openlayers</a>
+      <a href="https://zhoubichuan.github.io/web-cesium/">cesium</a>
+      <a href="https://zhoubichuan.github.io/web-three/">three</a>
+      <a href="https://zhoubichuan.github.io/web-webgl/">webgl</a>
+      <a href="https://zhoubichuan.github.io/web-mysql/">mysql</a>
+      <a href="https://zhoubichuan.github.io/web-nginx/">nginx</a>
+      <a href="https://zhoubichuan.github.io/web-redis/">redis</a>
+      <a href="https://zhoubichuan.github.io/web-mongodb/">mongodb</a>
+      <a href="https://zhoubichuan.github.io/web-browser/">browser</a>
+      <a href="https://zhoubichuan.github.io/web-arithmetic/">arithmetic</a>
+      <a href="https://zhoubichuan.github.io/web-javascript/">javascript</a>
+      <a href="https://zhoubichuan.github.io/web-typescript/">typescript</a>
+      <a href="https://zhoubichuan.github.io/web-mobile/">mobile</a>
+      <a href="https://zhoubichuan.github.io/web-node/">node</a>
+      <a href="https://zhoubichuan.github.io/web-devops/">devops</a>
+      <a href="https://zhoubichuan.github.io/web-docker/">docker</a>
+      <a href="https://zhoubichuan.github.io/web-tools/">tools</a>
+      <a href="https://zhoubichuan.github.io/web-koa/">koa</a>
+      <a href="https://zhoubichuan.github.io/web-express/">express</a>
+      <a href="https://zhoubichuan.github.io/web-egg/">egg</a>
+      <a href="https://zhoubichuan.github.io/web-java/">java</a>
+      <a href="https://zhoubichuan.github.io/jump/">jump</a>
+      <a href="https://zhoubichuan.github.io/resume/">resume</a>
+      <a href="https://zhoubichuan.github.io/classroom/">classroom</a>
+      <a href="https://zhoubichuan.github.io/">/</a>
+      <a href="https://zhoubichuan.github.io/website/">website</a>
+      <a href="https://zhoubichuan.github.io/zhoubichuan-vue-ui/">vue-ui</a>
+      <a href="https://github.com/zhoubichuan/antdpro-express"
+        >antdpro-express</a
+      >
+      <a href="https://zhoubichuan.github.io/antdpro-demo/">antdpro-demo</a>
+      <a href="https://zhoubichuan.github.io/abstract-umi/">abstract-umi</a>
+      <a href="https://zhoubichuan.github.io/abstract-react/">abstract-react</a>
+      <a href="https://zhoubichuan.github.io/abstract-vue/">abstract-vue</a>
+      <a href="https://zhoubichuan.github.io/abstract-express/"
+        >abstract-express</a
+      >
+      <a href="https://zhoubichuan.github.io/abstract-egg/">abstract-egg</a>
+      <a href="https://zhoubichuan.github.io/order/">order</a>
+      <a href="https://zhoubichuan.github.io/zhoubichuan-react-ui/">react-ui</a>
+      <a href="https://zhoubichuan.github.io/umi-blog/">umi-blog</a>
+      <a href="https://zhoubichuan.github.io/wechat-react/">wechat-react</a>
+      <a href="https://zhoubichuan.github.io/wechat-vue/">wechat-vue</a>
+      <a href="https://zhoubichuan.github.io/flybird/"></a>
+      <a href="https://zhoubichuan.github.io/managment/">managment</a>
+      <a href="https://zhoubichuan.github.io/elementsell/">elementsell</a>
+      <a href="https://github.com/zhoubichuan/note-backend"></a>
+      <a href="https://zhoubichuan.github.io/force/">force</a>
+      <a href="https://github.com/zhoubichuan/note-frontend">note-frontend</a>
+      <a href="https://github.com/zhoubichuan/note-backend">note-backend</a>
+    </div>
   </div>
 </template>
+<style>
+a {
+  width: 20%;
+  min-width: 100px;
+  display: block;
+  float: left;
+  line-height: 40px;
+  text-align: left;
+}
+</style>
